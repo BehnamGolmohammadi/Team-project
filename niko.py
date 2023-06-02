605640f496418966543650699bdb247a54760f9c
NikoDadfarma <nikou.engineer@gmail.com>
2023-06-02 10:57:02 +0330

    division by niko

--- niko.py
+++ niko.py
@@ -1,2 +1,12 @@
-def division():
-    pass
+def div_func(num1,num2,division):
+    '''for division num1 and num2'''
+    if num2==0:
+        return "can't division to zero"
+    return num1/num2
+num1=float(input())
+num2=float(input())
+operator =input()
+if operator=='/':
+    print(div_func(num1, num2, division))
+
+
